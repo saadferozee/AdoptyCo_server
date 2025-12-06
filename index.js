1050bd40bdf2c4dd265dcf13df98255a5ee1a880
saadferozee <saadferozee@gmail.com>
2025-12-06 18:08:56 +0600

    added new domain in cors/origin, all set

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(cors({
     origin: [
         "http://localhost:5173",
-        "https://adoptyco.netlify.app"
+        "https://adoption-web-by-saadferozee.web.app"
     ],
     credentials: true
 })); 
